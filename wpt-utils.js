693e01d0b0df6244a55be173c09b893073921f76
Simon Hearne <shearne@akamai.com>
2018-02-20 09:46:11 +0000

    Promisify wptUtils function

--- wpt-utils.js
+++ wpt-utils.js
@@ -11,38 +11,41 @@ const admzip = require('adm-zip');
 */
 
 module.exports = function wptUtils(testId) {
-    const testsDir = 'tests';
-    const wpt = new webpagetest();
-    
-    wpt.getTestStatus(testId, {request: 12345}, (err,data) => {
-        if (err) throw new Error(err);
-        else if (data.statusCode == 200) {
-            createDir(testId);
-        }
-        else {
-            return data.statusCode;
-        }
-    });
-    
-
-    const createDir = function() {
-        let dir = path.join(testsDir,testId);
-        let tmpdir = path.join(dir,'tmp');
+    return new Promise((resolve,reject) => {
+        const testsDir = 'tests';
+        const wpt = new webpagetest();
         
-        if (fs.existsSync(dir)){
-            fs.removeSync(dir)
-        }
-        fs.mkdirSync(dir);
-        fs.mkdirSync(tmpdir);
-        
-        /* first get filmstrip images somewhere */
-        wpt.getFilmstrip(testId,{},(err,data)=>{
+        wpt.getTestStatus(testId, {request: 12345}, (err,data) => {
             if (err) throw new Error(err);
-            let zip = new admzip(data);
-            zip.extractAllTo(tmpdir,true);
-            heatmap(tmpdir,dir).then(()=>{
-                fs.removeSync(tmpdir);
-            });
+            else if (data.statusCode == 200) {
+                createDir(testId);
+            }
+            else {
+                return data.statusCode;
+            }
         });
-    }
+        
+
+        const createDir = function() {
+            let dir = path.join(testsDir,testId);
+            let tmpdir = path.join(dir,'tmp');
+            
+            if (fs.existsSync(dir)){
+                fs.removeSync(dir)
+            }
+            fs.mkdirSync(dir);
+            fs.mkdirSync(tmpdir);
+            
+            /* first get filmstrip images somewhere */
+            wpt.getFilmstrip(testId,{},(err,data)=>{
+                if (err) throw new Error(err);
+                let zip = new admzip(data);
+                zip.extractAllTo(tmpdir,true);
+                heatmap(tmpdir,dir).then(()=>{
+                    fs.removeSync(tmpdir);
+                    return resolve(testId);
+                });
+            });
+        }
+    })
 }
