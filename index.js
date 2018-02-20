782a52343a76cf319680021deb40398e26fd62ba
Simon Hearne <shearne@akamai.com>
2018-02-20 09:39:00 +0000

    Add some test cases for experimentation

--- index.js
+++ index.js
@@ -2,5 +2,16 @@ const heatmap = require('./heatmap');
 const wptutils = require('./wpt-utils');
 
 
-var testId = "180219_13_4263ea10ce6841c6e417ace70136c98b";
-wptutils(testId);
+var testId = "180219_13_4263ea10ce6841c6e417ace70136c98b"; // britishairways
+var testId = "180220_K7_33c53d404a902d19c13eb67b9fae355c"; // dailymail
+var testId = "180220_R9_9f8c3a8e7cb713dc79141645f3e1e313"; // ferrari
+
+let testIds = [
+    "180219_13_4263ea10ce6841c6e417ace70136c98b", // britishairways
+    "180220_K7_33c53d404a902d19c13eb67b9fae355c", // dailymail
+    "180220_R9_9f8c3a8e7cb713dc79141645f3e1e313" // ferrari
+];
+for (var i in testIds) {
+    console.log(testIds[i]);
+    wptutils(testIds[i]);
+}
