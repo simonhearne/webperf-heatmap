b0279245102362eb3676e61faf3a1817dda73dad
Simon Hearne <shearne@akamai.com>
2018-02-20 10:15:15 +0000

    Tidy up test cases

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const heatmap = require('./heatmap');
 const wptutils = require('./wpt-utils');
 
-
-var testId = "180219_13_4263ea10ce6841c6e417ace70136c98b"; // britishairways
-var testId = "180220_K7_33c53d404a902d19c13eb67b9fae355c"; // dailymail
-var testId = "180220_R9_9f8c3a8e7cb713dc79141645f3e1e313"; // ferrari
-
 let testIds = [
     "180219_13_4263ea10ce6841c6e417ace70136c98b", // britishairways
     "180220_K7_33c53d404a902d19c13eb67b9fae355c", // dailymail
