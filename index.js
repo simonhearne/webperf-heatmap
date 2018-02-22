8b54d04c0d76a692e8d65230d91abc8c066309b6
Simon Hearne <shearne@akamai.com>
2018-02-22 15:47:31 +0000

    Add API endpoints

--- index.js
+++ index.js
@@ -1,14 +1,69 @@
 const heatmap = require('./heatmap');
 const wptutils = require('./wpt-utils');
+const express = require('express');
+const path = require('path');
 
-let testIds = [
-    "180219_13_4263ea10ce6841c6e417ace70136c98b", // britishairways
-    "180220_K7_33c53d404a902d19c13eb67b9fae355c", // dailymail
-    "180220_R9_9f8c3a8e7cb713dc79141645f3e1e313" // ferrari
-];
-for (var i in testIds) {
-    console.log("Submitting",testIds[i]);
-    wptutils(testIds[i]).then(()=>{
-        console.log("Complete",testIds[i]);
-    });
-}
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const url = require('url');
+
+const defaultServer = 'www.webpagetest.org';
+
+/*
+wptutils.createHeatmap("180220_R9_9f8c3a8e7cb713dc79141645f3e1e313",path.join('public','tests')).then(()=>{
+    console.log("Complete",testIds[i]);
+});
+*/
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(compression())
+
+app.use(express.static('public'))
+
+let port = 3123;
+
+const errMsg = {"status": "ERROR"};
+
+app.listen(port, () => {
+    app.get('/locations', (req,res) => {
+        let server = (req.query.server?req.query.server:defaultServer);
+        wptutils.getLocations(server).then((locations) => {
+            res.json(locations);
+        })
+    })
+    app.get('/status', (req, res) => {
+        console.log(req.query);
+        server = (req.query.server?req.query.server:defaultServer);
+        if (!req.query.test) {
+            res.json(errMsg);
+        } else {
+            let testId = req.query.test;
+            wptutils.getStatus(testId,server).then((response)=>{
+                res.json(response);
+            })
+        }
+    })
+    app.get('/result', (req,res) => {
+        console.log(req.query);
+        if (!req.query.test) {
+            res.json(errMsg);
+        } else {
+            wptutils.getResultSummary(req.query.test).then((result) => {
+                res.json(result)
+            })
+        }
+    })
+    app.post('/submit', (req, res) => {
+        console.log(req.query);
+        if (!req.body.url) {
+            res.json(errMsg);
+        } else {
+            let url = req.body.url;
+            wptutils.submitTest(testId).then((response)=>{
+                res.json(response);
+            })
+        }
+	});
+})
--- wpt-utils.js
+++ wpt-utils.js
@@ -4,30 +4,72 @@ const webpagetest = require('webpagetest');
 const heatmap = require('./heatmap');
 const admzip = require('adm-zip');
 
+const defaultServer = 'www.webpagetest.org';
+
 /* For submitting tests:
  * set screenshot size to full (probably new code in WPT-api)
  * set image quality to 100
  * set desktop resolution to something reasonable?
 */
 
-module.exports = function wptUtils(testId) {
+getStatus = function(testId) {
+    return new Promise((resolve)=>{
+        const wpt = new webpagetest();
+        wpt.getTestStatus(testId, {request: 12345}, (err,data) => {
+            if (err) throw new Error(err);
+            return resolve(data);
+        })
+    })
+}
+
+submitTest = function(url,server,location,opts) {
+    let iq = (opts.iq==undefined?100:opts.iq);
+    let res = (opts.res==undefined?"1280*1024":opts.res);
+    /* ---- */
+}
+
+getResultSummary = function(testId,server=null,requests=false) {
+    return new Promise((resolve) => {
+        let opts = {};
+        opts.requests = requests;
+        server = (server==null?defaultServer:server)
+        const wpt = new webpagetest(server);
+        wpt.getTestResults(testId, opts=opts, (err,data) => {
+            if (err) throw new Error(err);
+            return resolve(data);
+        })
+    })
+} 
+
+getLocations = function(server) {
+    return new Promise((resolve) => {
+        const wpt = new webpagetest(server);
+        wpt.getLocations((err,data) => {
+            if (err) throw new Error(err);
+            return resolve(data);
+        })
+    })
+}
+
+createHeatmap = function(testId,testDir) {
     return new Promise((resolve,reject) => {
-        const testsDir = 'tests';
+        if (testDir==undefined) {
+            let testDir = path.join('public','tests');
+        }
         const wpt = new webpagetest();
         
         wpt.getTestStatus(testId, {request: 12345}, (err,data) => {
             if (err) throw new Error(err);
             else if (data.statusCode == 200) {
-                createDir(testId);
+                generateHeatmap(testId,testDir);
             }
             else {
                 return data.statusCode;
             }
         });
         
-
-        const createDir = function() {
-            let dir = path.join(testsDir,testId);
+        const generateHeatmap = function(testId,testDir) {
+            let dir = path.join(testDir,testId);
             let tmpdir = path.join(dir,'tmp');
             
             if (fs.existsSync(dir)){
@@ -48,4 +90,12 @@ module.exports = function wptUtils(testId) {
             });
         }
     })
+}
+
+return module.exports = {
+    createHeatmap: createHeatmap,
+    getStatus: getStatus,
+    submitTest: submitTest,
+    getLocations:getLocations,
+    getResultSummary
 }
